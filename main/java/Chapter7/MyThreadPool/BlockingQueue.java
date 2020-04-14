42a18ca990566d21dc6f69dc991e322f49b99935
shitsurei <lfgewj21345@163.com>
2020-04-14 16:14:27 +0800

    线程池代码注释

--- main/java/Chapter7/MyThreadPool/BlockingQueue.java
+++ main/java/Chapter7/MyThreadPool/BlockingQueue.java
@@ -9,34 +9,38 @@ import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.ReentrantLock;
 
 @Slf4j
+/**
+ * 阻塞队列
+ * 作为外部线程使用线程池中的线程对象执行任务和线程池中的线程对象消费任务之间的缓冲组件
+ */
 public class BlockingQueue<T> {
     //    任务队列
-    private Deque<T> deque = new ArrayDeque<>();
-    //    锁
+    private Deque<T> taskQueue = new ArrayDeque<>();
+    //    锁（保证多个线程在【从队列中获取任务（队列头获取）】或【向队列中添加任务（队列尾添加）】时不会产生线程不安全的问题）
     private ReentrantLock lock = new ReentrantLock();
-    //    生产者条件变量
+    //    生产者条件变量（当任务队列已满时，添加任务的线程需要陷入阻塞，等待任务队列被消费后再唤醒添加）
     private Condition fullWaitSet = lock.newCondition();
-    //    消费者条件变量
+    //    消费者条件变量（当任务队列为空时，获取任务的线程需要陷入阻塞，等待任务队列添加新任务后再唤醒获取）
     private Condition emptyWaitSet = lock.newCondition();
-    //    容量
+    //    任务队列最大容量
     private int capacity;
 
     public BlockingQueue(int capacity) {
         this.capacity = capacity;
     }
 
-    //    带超时的阻塞等待
+    //    线程【带超时】的获取任务，任务队列为空时线程阻塞等待，等待超时后返回null
     public T poll(long timeout, TimeUnit unit) {
         lock.lock();
         try {
             long nanos = unit.toNanos(timeout);
-            while (deque.isEmpty()) {
-//                返回的剩余时间
+            while (taskQueue.isEmpty()) {
+//                返回的剩余时间（等待时间-经过时间）
                 long rest = emptyWaitSet.awaitNanos(nanos);
                 if (rest <= 0)
                     return null;
             }
-            T t = deque.removeFirst();
+            T t = taskQueue.removeFirst();
             fullWaitSet.signalAll();
             return t;
         } catch (InterruptedException e) {
@@ -47,14 +51,14 @@ public class BlockingQueue<T> {
         return null;
     }
 
-    //    阻塞获取
+    //    线程获取任务，任务队列为空时线程阻塞等待
     public T take() {
         lock.lock();
         try {
-            while (deque.isEmpty()) {
+            while (taskQueue.isEmpty()) {
                 emptyWaitSet.await();
             }
-            T t = deque.removeFirst();
+            T t = taskQueue.removeFirst();
             fullWaitSet.signalAll();
             return t;
         } catch (InterruptedException e) {
@@ -65,15 +69,15 @@ public class BlockingQueue<T> {
         return null;
     }
 
-    //    阻塞添加
+    //    线程添加任务，任务队列已满时线程阻塞等待
     public void put(T task) {
         lock.lock();
         try {
-            while (deque.size() == capacity) {
+            while (taskQueue.size() == capacity) {
                 fullWaitSet.await();
                 log.info("等待加入任务队列.... {}", task);
             }
-            deque.addLast(task);
+            taskQueue.addLast(task);
             log.info("加入任务队列，{}", task);
             emptyWaitSet.signalAll();
         } catch (InterruptedException e) {
@@ -83,17 +87,17 @@ public class BlockingQueue<T> {
         }
     }
 
-    //    带超时时间的阻塞添加
+    //    线程【带超时】的添加任务，任务队列已满时线程阻塞等待，等待超时后返回false表示添加失败，添加成功返回true
     public boolean offer(T task, long timeout, TimeUnit unit) {
         lock.lock();
         try {
-            while (deque.size() == capacity) {
+            while (taskQueue.size() == capacity) {
                 log.info("等待加入任务队列.... {}", task);
                 long rest = fullWaitSet.awaitNanos(unit.toNanos(timeout));
                 if (rest <= 0)
                     return false;
             }
-            deque.addLast(task);
+            taskQueue.addLast(task);
             log.info("加入任务队列，{}", task);
             emptyWaitSet.signalAll();
         } catch (InterruptedException e) {
@@ -104,27 +108,28 @@ public class BlockingQueue<T> {
         return true;
     }
 
-    //    获取大小
-    public int size() {
+    //    线程【可自定义拒绝策略】的添加任务，任务队列已满时，执行传入的解决策略
+    public void tryPut(RejectPolicy<T> rejectPolicy, T task) {
         lock.lock();
         try {
-            return capacity;
+//            判断队列是否已满
+            if (taskQueue.size() == capacity) {
+                rejectPolicy.reject(this, task);
+            } else {
+                taskQueue.addLast(task);
+                log.info("加入任务队列，{}", task);
+                emptyWaitSet.signalAll();
+            }
         } finally {
             lock.unlock();
         }
     }
 
-    public void tryPut(RejectPolicy<T> rejectPolicy, T task) {
+    //    获取队列中等待任务的数量
+    public int size() {
         lock.lock();
         try {
-//            判断队列是否已满
-            if (deque.size() == capacity) {
-                rejectPolicy.reject(this, task);
-            } else {
-                deque.addLast(task);
-                log.info("加入任务队列，{}", task);
-                emptyWaitSet.signalAll();
-            }
+            return capacity;
         } finally {
             lock.unlock();
         }
--- main/java/Chapter7/MyThreadPool/ThreadPool.java
+++ main/java/Chapter7/MyThreadPool/ThreadPool.java
@@ -6,6 +6,9 @@ import java.util.HashSet;
 import java.util.concurrent.TimeUnit;
 
 @Slf4j
+/**
+ * 线程池
+ */
 public class ThreadPool {
     //    任务队列
     private BlockingQueue<Runnable> taskQueue;
@@ -13,11 +16,11 @@ public class ThreadPool {
     private HashSet<Worker> workers = new HashSet<>();
     //    核心线程数
     private int coreSize;
-    //    获取任务的超时时间
-    private long timeout;
-    private TimeUnit unit;
     //    拒绝策略
     private RejectPolicy<Runnable> rejectPolicy;
+    //    获取任务的超时时间（超时时间内如果任务队列中获取不到任务，则该线程对象自动回收）
+    private long timeout;
+    private TimeUnit unit;
 
     public ThreadPool(int coreSize, long timeout, TimeUnit unit, int capacity, RejectPolicy<Runnable> rejectPolicy) {
         this.coreSize = coreSize;
@@ -27,6 +30,9 @@ public class ThreadPool {
         this.taskQueue = new BlockingQueue<>(capacity);
     }
 
+    /**
+     * 线程的包装类
+     */
     class Worker extends Thread {
         private Runnable task;
 
@@ -42,13 +48,14 @@ public class ThreadPool {
                     log.info("正在执行任务,{}", task);
                     task.run();
                 } finally {
-//            当task执行完毕，从任务队列获取任务并执行
+//            当task执行完毕，从任务队列获取正在等待的新任务执行
 //                    策略1：take方法为无超时时间的等待从任务队列中获取新任务
 //                    task = taskQueue.take();
 //                    策略2：poll方法为有超时时间的等待从任务队列中获取新任务
                     task = taskQueue.poll(timeout, unit);
                 }
             }
+//            超时时间内没有从任务队列中获取任务，释放该线程
             synchronized (workers) {
                 log.info("worker被移除,{}", this);
                 workers.remove(this);
@@ -56,15 +63,21 @@ public class ThreadPool {
         }
     }
 
+    //    线程池执行执行任务
     public void execute(Runnable task) {
+//        线程集合为HashSet，需要保证线程安全
         synchronized (workers) {
-//        当任务数没有超过coreSize时，直接交给worker对象执行，超过时加入任务队列缓存
+//        当任务数没有超过coreSize时
             if (workers.size() < coreSize) {
+//        直接新建Worker对象交给其执行
                 Worker worker = new Worker(task);
                 log.info("新增worker，{},{}", worker, task);
-                worker.start();
+//                将新创建的线程对象加入线程集合，后面复用
                 workers.add(worker);
+//                启动线程
+                worker.start();
             } else {
+//        当任务数超过coreSize时暂存入任务队列等待线程空闲时执行，同时传入拒绝策略，处理当任务队列已满时的处理逻辑
                 taskQueue.tryPut(rejectPolicy, task);
             }
         }
